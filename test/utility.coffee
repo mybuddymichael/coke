032819ef8de6966da4749b78c2c622edac6b6a91
Michael Hanson <michael.b.hanson@gmail.com>
2012-03-18 21:07:44 -0700

    Add test for Array.last

--- test/utility.coffee
+++ test/utility.coffee
@@ -3,3 +3,6 @@ $ ->
   test 'utility methods', 2, ->
     equal 1.clamp(3, 5), 3, 'clamp sets a lower number to min'
     equal 7.clamp(3, 5), 5, 'clamp sets a higher number to max'
+
+  test 'Array.last', 1, ->
+    equal [1,2,3].last(), 3, 'Array.last retrieves the last element'
--- test/utility.js
+++ test/utility.js
@@ -5,6 +5,9 @@
       equal(1..clamp(3, 5), 3, 'clamp sets a lower number to min');
       return equal(7..clamp(3, 5), 5, 'clamp sets a higher number to max');
     });
+    return test('Array.last', 1, function() {
+      return equal([1, 2, 3].last(), 3, 'Array.last retrieves the last element');
+    });
   });
 
 }).call(this);
