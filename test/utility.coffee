4a5fac79cb057318792480220a68331331d2373e
Michael Hanson <michael.b.hanson@gmail.com>
2012-03-18 21:08:04 -0700

    Clean up existing tests

--- test/utility.coffee
+++ test/utility.coffee
@@ -1,6 +1,8 @@
 $ ->
 
-  test 'utility methods', 2, ->
+  module 'utilities'
+
+  test 'Number.clamp', 2, ->
     equal 1.clamp(3, 5), 3, 'clamp sets a lower number to min'
     equal 7.clamp(3, 5), 5, 'clamp sets a higher number to max'
 
--- test/utility.js
+++ test/utility.js
@@ -1,7 +1,8 @@
 (function() {
 
   $(function() {
-    return test('utility methods', 2, function() {
+    module('utilities');
+    test('Number.clamp', 2, function() {
       equal(1..clamp(3, 5), 3, 'clamp sets a lower number to min');
       return equal(7..clamp(3, 5), 5, 'clamp sets a higher number to max');
     });
