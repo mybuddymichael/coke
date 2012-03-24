b0efd40aa4fe4acf05f613c2f32423fee2e29458
Michael Hanson <michael.b.hanson@gmail.com>
2012-03-24 15:25:26 -0700

    Add more getImageXFromPosition tests and test x and y attributes

--- test/character.coffee
+++ test/character.coffee
@@ -2,9 +2,37 @@ $ ->
 
   module 'Character'
 
-  test 'getImageXFromPosition', 4, ->
+  test 'x is readable', 1, ->
+    testChar = new Coke.Character(10)
+    equal testChar.x, 10
+
+  test 'y is readable', 1, ->
+    testChar = new Coke.Character(0, 11)
+    equal testChar.y, 11
+
+  test 'getImageXFromPosition square coordinates', 4,  ->
     testChar = new Coke.Character
     equal testChar.getImageXFromPosition('up', 0, 0), 0
     equal testChar.getImageXFromPosition('down', 0, 0), 32
     equal testChar.getImageXFromPosition('left', 0, 0), 32 * 2
     equal testChar.getImageXFromPosition('right', 0, 0), 32 * 3
+
+  test 'getImageXFromPosition up off-grid', 2, ->
+    testChar = new Coke.Character
+    equal testChar.getImageXFromPosition('up', 0, 31), 128
+    equal testChar.getImageXFromPosition('up', 0, 63), 160
+
+  test 'getImageXFromPosition down off-grid', 2, ->
+    testChar = new Coke.Character
+    equal testChar.getImageXFromPosition('down', 0, 33), 192
+    equal testChar.getImageXFromPosition('down', 0, 1), 224
+
+  test 'getImageXFromPosition left off-grid', 2, ->
+    testChar = new Coke.Character
+    equal testChar.getImageXFromPosition('left', 31, 0), 256
+    equal testChar.getImageXFromPosition('left', 1, 0), 64
+
+  test 'getImageXFromPosition right off-grid', 2, ->
+    testChar = new Coke.Character
+    equal testChar.getImageXFromPosition('right', 1, 0), 288
+    equal testChar.getImageXFromPosition('right', 31, 0), 96
--- test/character.js
+++ test/character.js
@@ -2,13 +2,47 @@
 
   $(function() {
     module('Character');
-    return test('getImageXFromPosition', 4, function() {
+    test('x is readable', 1, function() {
+      var testChar;
+      testChar = new Coke.Character(10);
+      return equal(testChar.x, 10);
+    });
+    test('y is readable', 1, function() {
+      var testChar;
+      testChar = new Coke.Character(0, 11);
+      return equal(testChar.y, 11);
+    });
+    test('getImageXFromPosition square coordinates', 4, function() {
       var testChar;
       testChar = new Coke.Character;
       equal(testChar.getImageXFromPosition('up', 0, 0), 0);
       equal(testChar.getImageXFromPosition('down', 0, 0), 32);
       equal(testChar.getImageXFromPosition('left', 0, 0), 32 * 2);
       return equal(testChar.getImageXFromPosition('right', 0, 0), 32 * 3);
+    });
+    test('getImageXFromPosition up off-grid', 2, function() {
+      var testChar;
+      testChar = new Coke.Character;
+      equal(testChar.getImageXFromPosition('up', 0, 31), 128);
+      return equal(testChar.getImageXFromPosition('up', 0, 63), 160);
+    });
+    test('getImageXFromPosition down off-grid', 2, function() {
+      var testChar;
+      testChar = new Coke.Character;
+      equal(testChar.getImageXFromPosition('down', 0, 33), 192);
+      return equal(testChar.getImageXFromPosition('down', 0, 1), 224);
+    });
+    test('getImageXFromPosition left off-grid', 2, function() {
+      var testChar;
+      testChar = new Coke.Character;
+      equal(testChar.getImageXFromPosition('left', 31, 0), 256);
+      return equal(testChar.getImageXFromPosition('left', 1, 0), 64);
+    });
+    return test('getImageXFromPosition right off-grid', 2, function() {
+      var testChar;
+      testChar = new Coke.Character;
+      equal(testChar.getImageXFromPosition('right', 1, 0), 288);
+      return equal(testChar.getImageXFromPosition('right', 31, 0), 96);
     });
   });
 
